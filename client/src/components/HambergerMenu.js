c8ad5c24ceb7dfca7f9dea1de600cc68791be0dd
SatyamKumar <kumarsatyam04.2000@gmail.com>
2023-07-15 14:42:25 +0530

    commit all

--- client/src/components/HambergerMenu.js
+++ client/src/components/HambergerMenu.js
@@ -1,13 +1,32 @@
-import React, { useState } from 'react';
+import React, { useState,useRef, useEffect} from 'react';
 
 const HambergerMenu = () => {
     const [isOpen, setIsOpen] = useState(false);
+    const divRef = useRef(null);
 
     const toggleMenu = () => {
         setIsOpen(!isOpen);
       };
+
+      const handleNavbar=()=>{
+        setIsOpen(false)
+      }
+
+      const handleScreenClick  = (event)=>{
+        if(divRef.current && !divRef.current.contains(event.target)){
+          setIsOpen(false);
+        }
+      };
+
+      useEffect(()=>{
+        document.addEventListener('click',handleScreenClick );
+        return ()=>{
+        document.removeEventListener('click',handleScreenClick );
+        };
+      },[])
+
   return (
-    <div className="flex items-center">
+    <div ref={divRef} className="flex items-center">
     <button
       className="text-gray-600 focus:outline-none focus:text-gray-900 md:hidden"
       onClick={toggleMenu}
@@ -17,9 +36,9 @@ const HambergerMenu = () => {
       <div className={`w-6 h-0.5 bg-gray-600 mt-1.5 ${isOpen ? 'transform -rotate-45 -translate-y-1.5' : ''}`}></div>
     </button>
   
-    <nav className={`${isOpen ? 'block' : 'hidden'} absolute z-10 top-10 left-center md:relative md:top-auto md:left-auto lg:block bg-slate-100`}>
+    <nav  className={`${isOpen ? 'block' : 'hidden'} rounded-md  absolute z-10 top-10 md:left-center left-0 md:relative md:top-auto md:left-auto lg:block bg-slate-100`}>
       {/* Your menu items go here */}
-      <ul className="w-full md:w-auto flex flex-col md:flex-row justify-between md:gap-12">
+      <ul className=" md:w-auto w-[100px] m-4  flex flex-col md:flex-row justify-between md:gap-12" onClick={handleNavbar}>
         <li className="text-gray-600 hover:text-gray-900 hover:scale-110 flex-shrink-0">
           <a href="/#">Home</a>
         </li>
