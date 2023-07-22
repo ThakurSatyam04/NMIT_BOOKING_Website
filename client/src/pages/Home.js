82d5505f8fe86bc12153036feaa481140d8f3956
SatyamKumar <kumarsatyam04.2000@gmail.com>
2023-07-22 13:23:12 +0530

    commit all

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -28,61 +28,60 @@ const Home = ({loggedIn}) => {
   </div>
 </div>
 
-    <section id="about" className="w-full h-full flex flex-col md:flex-row justify-center items-center mx-5 md:mx-0 md:my-0 bg-blue-50 pb-10">
-      <div className='flex items-center justify-center flex-col'>
-        <div className="w-8/12 flex p-6 rounded-lg mt-10 gap-10">
-          <div>
-            <a href="#about" >
-              <img  className='rounded-lg shadow-md '
-              src={about_img}
-              alt="Sample image"/>
-            </a>
+<section id="about" className="w-full h-full flex flex-col md:flex-row justify-center items-center mx-5 md:mx-0 md:my-0 bg-blue-50 pb-10">
+  <div className='flex items-center justify-center flex-col'>
+    <div className="w-full md:w-8/12 flex flex-col md:flex-row p-6 rounded-lg mt-10 gap-10">
+      <div>
+        <a href="#about">
+          <img className='rounded-lg shadow-md ' src={about_img} alt="Sample image"/>
+        </a>
+      </div>
+      <div className="w-full md:w-11/12">
+        <h4 className="text-center md:text-left text-2xl font-extrabold text-gray-800 mb-4">
+          Welcome to our Equipment Booking Service
+        </h4>
+        <span className="text-gray-600">
+          We provide a seamless experience for students and faculty members to book equipment for various academic and extracurricular activities.
+          <ul>
+            <li>
+              Unlock the potential of your academic journey with our state-of-the-art equipment booking service!
+            </li>
+            <li>
+              Empowering students and faculty alike, our user-friendly platform ensures hassle-free equipment reservations.
+            </li>
+            <li>
+              Focus on your passions, and leave the equipment logistics to us, as we streamline the booking process for your convenience.
+            </li>
+          </ul>
+        </span>
+      </div>
+    </div>
+
+    <div className="w-full md:w-8/12">
+      <div className="w-full items-center before:mt-0.5 before:flex-1 before:border-t before:border-neutral-300 after:mt-0.5 after:flex-1 after:border-t after:border-neutral-300 scroll-smooth"> 
+        <div className="w-full flex flex-col md:flex-row max-w-6xl mx-auto mt-10 mb-6 justify-between gap-10 ">
+          <div className="w-full md:w-11/12 bg-[#fff] p-2 rounded-md shadow-xl border-white cursor-pointer hover:scale-110 transition smooth duration-500">
+            <h3 className="text-xl font-semibold text-gray-800 mb-2 underline">
+              Our Mission
+            </h3>
+            <p className="text-gray-600">
+              Our mission is to empower students and faculty members by providing easy access to high-quality equipment for their educational and creative pursuits.
+            </p>
           </div>
-          <div className=" w-11/12">
-            <h4 className="text-center text-2xl font-extrabold text-gray-800 mb-4">
-              Welcome to our Equipment Booking Service
-            </h4>
-            <span className="text-gray-600">
-              We provide a seamless experience for students and faculty members to book equipment for various academic and extracurricular activities.
-              <ul>
-                <li>
-                  Unlock the potential of your academic journey with our state-of-the-art equipment booking service!
-                </li>
-                <li>
-                  Empowering students and faculty alike, our user-friendly platform ensures hassle-free equipment reservations.
-                </li>
-                <li>
-                Focus on your passions, and leave the equipment logistics to us, as we streamline the booking process for your convenience.
-                </li>
-              </ul>
-            </span>
+          <div className="w-full md:w-11/12 bg-[#fff] p-2 rounded-md shadow-xl border-white cursor-pointer hover:scale-110 transition smooth duration-500">
+            <h3 className="text-xl font-semibold text-gray-800 mb-2 underline">
+              How It Works
+            </h3>
+            <p className="text-gray-600">
+              Our platform allows users to browse available equipment, check availability, and make reservations online. Simply create an account, search for the equipment you need, select a time slot, and book it with ease.
+            </p>
           </div>
         </div>
-
-          <div className="md:w-8/12">
-            <div className="w-full items-center before:mt-0.5 before:flex-1 before:border-t before:border-neutral-300 after:mt-0.5 after:flex-1 after:border-t after:border-neutral-300 scroll-smooth"> 
-            <div className="w-full flex max-w-6xl mx-auto mt-10 mb-6 justify-between gap-10 ">
-              <div className="w-11/12 bg-[#fff] p-2 rounded-md shadow-xl border-white cursor-pointer hover:scale-110 transition smooth duration-500">
-                <h3 className="text-xl font-semibold text-gray-800 mb-2 underline">
-                  Our Mission
-                </h3>
-                <p className="text-gray-600">
-                  Our mission is to empower students and faculty members by providing easy access to high-quality equipment for their educational and creative pursuits.
-                </p>
-              </div>
-              <div className="w-11/12 bg-[#fff] p-2 rounded-md shadow-xl border-white cursor-pointer hover:scale-110 transition smooth duration-500">
-                <h3 className="text-xl font-semibold text-gray-800 mb-2 underline">
-                  How It Works
-                </h3>
-                <p className="text-gray-600">
-                  Our platform allows users to browse available equipment, check availability, and make reservations online. Simply create an account, search for the equipment you need, select a time slot, and book it with ease.
-                </p>
-              </div>
-          </div>
-            </div>
-          </div>
       </div>
-    </section><hr />
+    </div>
+  </div>
+</section>
+<hr />
 
     <div className="ml-4 sm:ml-24 mt-6">
   <a href="#labs">
